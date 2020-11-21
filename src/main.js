45beb41fef93a27ae1f04982010f56a44605694e
Ezequiel <ezequieljfrias20@gmail.com>
2020-11-21 15:29:07 -0400

    Proyecto listo

--- src/main.js
+++ src/main.js
@@ -6,5 +6,47 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let valueCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suice = [
+    { color: "black", simbolo: "♠" },
+    { color: "black", simbolo: "♣" },
+    { color: "red", simbolo: "♥" },
+    { color: "red", simbolo: "♦" }
+  ];
+  let numberRandom = Math.trunc(Math.random() * valueCard.length);
+  let suiceRandom = Math.trunc(Math.random() * suice.length);
+
+  document.querySelector(".fondo").innerHTML = `
+  
+  <div class="card">
+    <div class="top">
+      <div class="symbol-top ${suice[suiceRandom].color}">${suice[suiceRandom].simbolo}</div>
+    </div>
+
+        <div class="middle">${valueCard[numberRandom]}</div>
+        <div class="bottom">
+          <div class="symbol-bottom ${suice[suiceRandom].color}">${suice[suiceRandom].simbolo}</div>
+        </div>
+  </div>
+  
+  
+  
+  
+  
+  
+  `;
 };
